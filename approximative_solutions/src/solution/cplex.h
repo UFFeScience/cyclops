408b919b434c3bd0b99aa8452fd0dd6fee0e97a9
fofolete <abitbol@gmail.com>
2020-05-02 16:35:27 -0300

    m

--- approximative_solutions/src/solution/cplex.h
+++ approximative_solutions/src/solution/cplex.h
@@ -13,7 +13,38 @@
 #ifndef APPROXIMATIVE_SOLUTIONS_SRC_CPLEX_H_
 #define APPROXIMATIVE_SOLUTIONS_SRC_CPLEX_H_
 
+#include <ilcplex/ilocplex.h>
 #include "src/solution/algorithm.h"
+
+
+//------------ Estrutura do ambiente cplex (ambiente, modelo e variaveis) -------------------
+struct CPLEX
+{
+  IloEnv   env;
+  IloModel model;
+  IloArray<IloArray<IloBoolVarArray>> x;
+  IloArray<IloArray<IloArray<IloArray<IloBoolVarArray>>>> r;
+  IloArray<IloArray<IloArray<IloArray<IloBoolVarArray>>>> w;
+  IloArray<IloArray<IloBoolVarArray>> y;
+  IloArray<IloBoolVarArray> yb;
+  IloArray<IloBoolVarArray> v;
+  IloNumVarArray z;
+  IloNumVarArray z_max;
+  
+  CPLEX(int n, int dd, int d, int m) :
+    env(),
+    model(env),
+    x(env, n),
+    r(env, n),
+    w(env, n),
+    y(env, d),
+    yb(env, d),
+    v(env, m),
+    z(env, m),
+    z_max(env, 1) {}
+ 
+};
+
 
 class Cplex : public Algorithm {
  public:
