25161ae95698317932c9bc58c2dda2456852774a
fofolete <abitbol@gmail.com>
2020-05-03 15:43:15 -0300

    1

--- approximative_solutions/src/solution/cplex.cc
+++ approximative_solutions/src/solution/cplex.cc
@@ -76,8 +76,7 @@ void Cplex::Run() {
   for (int i = 0; i < _n; i++) {
     Task*              task        = GetTaskPerId(static_cast<size_t>(i + 1));
     std::vector<File*> input_files = task->get_input_files();
-    cplx.r[i]                      =  IloArray<IloArray<IloArray<IloBoolVarArray>>>(cplx.env,
-        static_cast<int>(input_files.size()));
+    cplx.r[i]                      =  IloArray<IloArray<IloArray<IloBoolVarArray>>>(cplx.env, static_cast<int>(input_files.size()));
 
     for (int j = 0; j < static_cast<int>(input_files.size()); j++) {
       cplx.r[i][j] = IloArray<IloArray<IloBoolVarArray>>(cplx.env, _m);
@@ -94,6 +93,34 @@ void Cplex::Run() {
       }
     }
   }
+
+
+// variaveis de escrita
+  // W_DJPT => a tarefa I que esta na maquina J, comeca a escrever o seu D-esimo dado de entrada
+  // (note que nao eh o dado de indice D) a partir da maquina P no periodo T
+  for(int i=0; i < _n; i++)
+    {
+      cplx.w[i] =  IloArray<IloArray<IloArray<IloBoolVarArray>>>(cplx.env, data.d_out_tam[i]);
+      for(int j=0; j < data.d_out_tam[i]; j++)
+	{
+	  cplx.w[i][j] = IloArray<IloArray<IloBoolVarArray>>(cplx.env, _m);
+	  for(int k=0; k < _m; k++)
+	    {
+	      cplx.w[i][j][k] = IloArray<IloBoolVarArray>(cplx.env, _m);
+	      for(int l=0; l < _m; l++)
+		{
+		  cplx.w[i][j][k][l] = IloBoolVarArray(cplx.env, _t);
+		  for(int m=0; m < _t; m++)
+		    {
+		      //cout<<"cria w_"<<i<<"_"<<j<<"_"<<k<<"_"<<l<<"_"<<m<<endl;
+		      sprintf (var_name, "w_%d_%d_%d_%d_%d", (int)i,(int)j, (int)k, (int)l, (int)m);            // nome da variavel
+		      cplx.w[i][j][k][l][m] = IloBoolVar(cplx.env, var_name);                                   // aloca variavel 
+		      cplx.model.add(cplx.w[i][j][k][l][m]);                                                    // adiciona variavel ao modelo
+		    }
+		}
+	    }
+	}
+    }
 
 
   IloCplex solver(cplx.model);                        // declara variável "solver" sobre o modelo a ser solucionado
