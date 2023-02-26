c6a2c9b81faab11d7cdd12565489289ae0dc96a4
Rodrigo Alves Prado da Silva <rodrigo.raps@gmail.com>
2023-02-26 16:18:26 -0300

    Updating `run-algorithm`.

--- heuristic_and_exact_solutions/shell/run-algorithm.sh
+++ heuristic_and_exact_solutions/shell/run-algorithm.sh
@@ -60,14 +60,4 @@ $PROG --tasks_and_files `pwd`/input/tasks_and_files/$TASK_AND_FILES \
 #  --log_dir=`pwd`/log
 cd shell
 
-# cd ..
-# #echo  GLOG_log_dir=`pwd`/log
-# export GLOG_log_dir=`pwd`/log
-# #export OMP_NUM_THREADS=4
-# ./bin/wf_security_greedy.x  --tasks_and_files `pwd`/input/tasks_and_files/3_toy_5_A.dag \
-#   --cluster `pwd`/input/clouds/cluster2.vcl \
-#   --conflict_graph `pwd`/input/conflict_graph/3_toy_5_A.scg \
-#   --algorithm cplex \
-#   --cplex_output_file `pwd`/output/3_toy_5_A.lp
-
-# cd shell
+# TODO: Make more flexible, receive the algorithm name, and create the name of the log file and directory
