b964335d3cd5d72086d4bf23f75d23e89605e9b8
Rodrigo Alves Prado da Silva <rodrigo_prado@id.uff.br>
2024-01-07 11:23:44 -0300

    feat: adjusting execution parameters

--- heuristic_and_exact_solutions/execute-Synthetic_10_B-grasp.sh
+++ heuristic_and_exact_solutions/execute-Synthetic_10_B-grasp.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-ALGORITHM=grch
+ALGORITHM=grasp
 
-INSTANCE=Synthetic_5_A
+INSTANCE=Synthetic_010_B
 
-PROGRAM=/home/vagrant/Wf-Security/bin/wf_security_greedy.x
-TASK_AND_FILES=/home/vagrant/Wf-Security/input/tasks_and_files/$INSTANCE.dag
-CLUSTER=/home/vagrant/Wf-Security/temp/clouds/1_1_4_2_1_4_homogeneous_cloud.vcl
-CONFLICT_GRAPH=/home/vagrant/Wf-Security/input/conflict_graph/$INSTANCE.scg
+PROGRAM=/home/wsl/git/Wf-Security/heuristic_and_exact_solutions/bin/wf_security_greedy.x
+TASK_AND_FILES=/home/wsl/git/Wf-Security/heuristic_and_exact_solutions/input/tasks_and_files/$INSTANCE.dag
+CLUSTER=/home/wsl/git/Wf-Security/heuristic_and_exact_solutions/temp/clouds/4_1_4_16_2_4_heterogeneous_cloud.vcl
+CONFLICT_GRAPH=/home/wsl/git/Wf-Security/heuristic_and_exact_solutions/input/conflict_graph/$INSTANCE.scg
 NUMBER_OF_INTERATION=100
 ALPHA_TIME=0.9
 ALPHA_BUDGET=0.05
 ALPHA_SECURITY=0.05
 NUMBER_OF_ALLOCATION_EXPERIMENTS=4
-LOG_DIR=/home/vagrant/Wf-Security/log/manual/grch/$ALPHA_TIME_$ALPHA_BUDGET_$ALPHA_SECURITY/1_1_4_2_1_4_homogeneous_cloud/0/$INSTANCE/
+LOG_DIR=/home/wsl/git/Wf-Security/heuristic_and_exact_solutions/log/manual/grch/$ALPHA_TIME_$ALPHA_BUDGET_$ALPHA_SECURITY/1_1_4_2_1_4_homogeneous_cloud/0/$INSTANCE/
 
 mkdir -p $LOG_DIR
 
