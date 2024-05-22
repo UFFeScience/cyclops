c9dc82914ec426641cecfe8fde38394b92d39b33
Rodrigo Alves Prado da Silva <rodrigo_prado@id.uff.br>
2024-05-22 00:01:59 -0300

    feat:reformatting and adding new information

--- heuristic_and_exact_solutions/src/main.cc
+++ heuristic_and_exact_solutions/src/main.cc
@@ -5,7 +5,7 @@
  * \author Rodrigo Alves Prado da Silva \<rodrigo_prado@id.uff.br\>
  * \copyright Fluminense Federal University (UFF)
  * \copyright Computer Science Department
- * \date 2020
+ * \date 2024
  *
  * This source file contains the \c main() function that reads the execution mode from the
  * command-line and calls the appropriate methods.
@@ -101,6 +101,8 @@ int main(int argc, char **argv) {
     DLOG(INFO) << "... algorithm picked-up ...";
 
     algorithm->ReadInputFiles(FLAGS_tasks_and_files, FLAGS_cluster, FLAGS_conflict_graph);
+    std::cout << "Number of Activation: " << algorithm->GetActivationSize() << std::endl;
+    std::cout << "Number of files: " << algorithm->GetFilesSize() << std::endl;
     algorithm->SetAlphas(FLAGS_alpha_time,
                          FLAGS_alpha_budget,
                          FLAGS_alpha_security,
