55d215d4478fc3cc0154bcdde3deff75ee0a24ef
Rodrigo Alves Prado da Silva <rodrigo_prado@id.uff.br>
2024-05-22 00:05:05 -0300

    fix:reformatting `requirement.h` file

--- heuristic_and_exact_solutions/src/model/requirement.h
+++ heuristic_and_exact_solutions/src/model/requirement.h
@@ -1,17 +1,18 @@
 /**
  * \file src/model/requirement.h
- * \brief Contains the \c Requirement class declaration.
+ * \brief Contains the \c Requirement class declaration
  *
- * \authors Rodrigo Alves Prado da Silva \<rodrigo_prado@id.uff.br\>
+ * \authors Rodrigo Alves Prado da Silva \<rodrigo.raps@gmail.com\>
  * \copyright Fluminense Federal University (UFF)
  * \copyright Computer Science Department
- * \date 2020
+ * \date 2024
  *
- * This header file contains the \c Requirement class.
+ * This header file contains the \c Requirement class
  */
 
 #ifndef APPROXIMATIVE_SOLUTIONS_SRC_MODEL_REQUIREMENT_H_
 #define APPROXIMATIVE_SOLUTIONS_SRC_MODEL_REQUIREMENT_H_
+
 
 #include <string>
 #include <iostream>
@@ -22,32 +23,33 @@
  * \brief Represents the requirement needed for the execution of the \c Activation in a Virtual Machine
  */
 class Requirement {
- public:
-  /// Parametrised constructor
-  explicit Requirement(const size_t id, const double max_value) : id_(id), max_value_(static_cast<int>(max_value)) { }
+public:
+    /// Parametrised constructor
+    explicit Requirement(const size_t id, const double max_value) : id_(id), max_value_(static_cast<int>(max_value)) {}
 
-  /// Getter for ID of the \c Requirement
-  [[nodiscard]] size_t get_id() const { return id_; }
+    /// Getter for ID of the \c Requirement
+    [[nodiscard]] size_t get_id() const { return id_; }
 
-  /// Getter for maximum integer value that a specific Requirement can have
-  [[nodiscard]] int get_max_value() const { return max_value_; }
+    /// Getter for maximum integer value that a specific Requirement can have
+    [[nodiscard]] int get_max_value() const { return max_value_; }
 
-  // Concatenate operator
-  friend std::ostream& operator<<(std::ostream& os, const Requirement& a) {
-    return a.write(os);
-  }
+    // Concatenate operator
+    friend std::ostream &operator<<(std::ostream &os, const Requirement &a) {
+        return a.write(os);
+    }
 
- private:
-  /// Print the \c Requirement object to the output stream
-  std::ostream& write(std::ostream& os) const {
-    return os << "Requirement[_id: " << id_ << ", " << "_max_value: " << max_value_ << "]";
-  }
+private:
+    /// Print the \c Requirement object to the output stream
+    std::ostream &write(std::ostream &os) const {
+        return os << "Requirement[_id: " << id_ << ", " << "_max_value: " << max_value_ << "]";
+    }
 
-  /// The ID of the \c Requirement
-  size_t id_;
+    /// The ID of the \c Requirement
+    size_t id_;
 
-  /// The maximum value that a \c Requirement can have
-  int max_value_;
+    /// The maximum value that a \c Requirement can have
+    int max_value_;
 };  // end of class Requirement
+
 
 #endif  // APPROXIMATIVE_SOLUTIONS_SRC_MODEL_REQUIREMENT_H_
